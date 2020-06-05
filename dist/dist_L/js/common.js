e54e9f34d698fec7473812e029dcc060e4a53908
landinialejandro <landinialejandro@gmail.com>
2020-06-05 16:22:49 -0300

    updating project

---
+++ dist/dist_L/js/common.js
@@ -0,0 +1,13 @@
+function mySort(theArray) {
+    theArray.sort(function (a, b) {
+      if (a.order > b.order) {
+        return 1;
+      }
+      if (a.order < b.order) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+    return theArray;
+  }
--- starter.js
+++ starter.js
@@ -1,163 +1,234 @@
 var fields_settings = [];
 $(function () {
-
-  $('#project_tree').jstree({
-    "core": {
-      "data": {
-        "url": "projects/project.json",
-        "dataType": "json" // needed only if you do not supply JSON headers
-      },
-      'check_callback': function (o, n, p, i, m) {
-        
-        if (m && m.dnd && m.pos !== 'i') {
-          return false;
-        }
-        if (o === "move_node" || o === "copy_node") {
-          if (this.get_node(n).parent === this.get_node(p).id) {
-            return false;
-          }
-        }
-        return true;
-      }
-    },
-    "types": {
-      "#": {
-        "max_children": 3,
-        "icon": "fas fa-project-diagram"
-      },
-      "group": {
-        "max_children": 20,
-        "max_depth": 20,
-        "icon": "far fa-object-group"
-      },
-      "table": {
-        "max_children": 20,
-        "max_depth": 20,
-        "icon": "fas fa-table"
-      },
-      "field": {
-        "max_children": 20,
-        "max_depth": 20,
-        "icon": "fas fa-stream"
-      }
-    },
-    'contextmenu': {
-      'items': function (node) {
-        return contextMenu(node, this);
-      }
-    },
-    "plugins": ["dnd", "search", "state", "types", "contextmenu"]
-  });
-  cosntructFieldsSettings();
+	$.get("projects/project.json")
+		.done(function (data) {
+			constructTree(data);
+		});
+	cosntructFieldsSettings();
 });
 
 $(".getjson").on('click', function (e) {
-  var tree = $('#project_tree').jstree(true);
-  var selTreeData = tree.get_selected(); // set flat:true to get all nodes in 1-level json
-  var treeData = tree.get_json(selTreeData, {
-    flat: false
-  }); // set flat:true to get all nodes in 1-level json
-  var childrens = tree.get_children_dom(selTreeData);
-  //var jsonData = JSON.stringify(treeData);
-  var text = '<span class = "right badge badge-success">'+ treeData.type + 
-            '</span><span class="right badge badge-danger">' + treeData.text + 
-            '</span><span class="right badge badge-warning">' + treeData.id +'</span>';
-  $('.card-title').html(text);
-  doForm(childrens);
+	var treeData = getJsonNode();
+	var jsonData = JSON.stringify(treeData);
+	console.log(jsonData);
+	updateTree();
+
+});
+$(".save-app-data").on('click', function (e) {
+
+	$('.form-node').each(function () {
+		console.log(this);
+		console.log(getJsonNode(this.dataset.nodeid));
+	});
 
 });
 
+function getJsonNode(id = '#', flat = false) {
+	var tree = $('#project_tree').jstree(true);
+	var nodeDataJson = tree.get_json(id, {
+		flat: flat
+	}); // set flat:true to get all nodes in 1-level json
+	return nodeDataJson;
+}
+
 function contextMenu(node, $this) { //create adtional context menu
-  var tmp = $.jstree.defaults.contextmenu.items();
-  delete tmp.create.action;
-  tmp.create.label = "New object";
-  tmp.create.submenu = {
-    "create_group": {
-      "separator_after": true,
-      "label": "Group",
-      "action": function (data) {
-        createNode(data, "group");
-      }
-    },
-    "create_table": {
-      "separator_after": true,
-      "label": "Table",
-      "action": function (data) {
-        createNode(data, "table");
-      }
-    },
-    "create_field": {
-      "label": "Field",
-      "action": function (data) {
-        createNode(data, "field");
-      }
-    }
-  };
-  if ($this.get_type(node) === "field_") {
-    delete tmp.create;
-  }
-  return tmp;
+	var tmp = $.jstree.defaults.contextmenu.items();
+	delete tmp.create.action;
+	tmp.create.label = "New object";
+	tmp.create.submenu = {
+		"create_group": {
+			"separator_after": true,
+			"label": "Group",
+			"action": function (data) {
+				createNode(data, "group");
+			}
+		},
+		"create_table": {
+			"separator_after": true,
+			"label": "Table",
+			"action": function (data) {
+				createNode(data, "table");
+			}
+		},
+		"create_field": {
+			"label": "Field",
+			"action": function (data) {
+				createNode(data, "field");
+			}
+		}
+	};
+	if ($this.get_type(node) === "field_") {
+		delete tmp.create;
+	}
+	return tmp;
 }
 
 function createNode(data, type) {
-  var inst = $.jstree.reference(data.reference);
-  var obj = inst.get_node(data.reference);
-  inst.create_node(obj, {
-    type: type
-  }, "last", function (new_node) {
-    setTimeout(function () {
-      inst.edit(new_node);
-    }, 0);
-  });
+	var inst = $.jstree.reference(data.reference);
+	var obj = inst.get_node(data.reference);
+
+	if (type === 'field') {
+		childs = fields_settings;
+	} else {
+		childs = [];
+	}
+
+	inst.create_node(obj, {
+		type: type,
+		text: "new_" + type + "_" + (obj.children.length + 1),
+		children: childs
+	}, "last", function (new_node) {
+		setTimeout(function () {
+			inst.edit(new_node);
+		}, 0);
+	});
 }
 
-function doForm(obj) {
-  var tree = $('#project_tree').jstree(true);
-  var html = '<form role="form">';
-  $(obj).each(function () {
-    var json_obj = tree.get_json(this.id);
-    //console.log(json_obj);
-    html += '<div class="form-group">' +
-      '<label for="' + this.id + '" class="">' + json_obj.text +
-      ': </label>';
-    if (this.type === 'text' || this.type === 'integer') {
-        var value = json_obj.data.user_value;
-        var placeholder = json_obj.data.placeholder;
-        if (this.lang){
-            json_obj.data.user_value.forEach(element =>{
-                value = element.text;
-                html +='<div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text">'+element.id+'</span></div>';
-                html +='<input type="text" class="form-control" placeholder="' + placeholder + '" value = "' + value + '" ></div>';
-            });
-        }else{
-            html += '<input type="text" class="form-control" name="' + this.id + '" id="' + this.id +
-              '" placeholder="' + placeholder + '" value = "' + value + '" >';
-        }
-    }
-    if (this.type === 'options') {
-      json_obj.children.forEach(element => {
-        html += '<select class="custom-select" name="' + this.id + '" id="' + this.id + '">';
-        element.children.forEach(element => {
-          html += '<option value="' + element.text + '">' + element.text + '</option>';
-        });
-        html += '</select>';
-      });
-    }
-    html += '</div>';
-  });
-  html += '</form>';
-  $('.field-options').html(html);
+function constructForm(obj) {
+	var html = '<form role="form">';
+	$(obj).each(function () {
+		var node_obj = getJsonNode(this.id);
+		html += '<div class="form-group">' +
+			'<label for="' + this.id + '" class="">' + node_obj.text +
+			': </label>';
+		if (this.type === 'text' || this.type === 'integer') {
+			var value = node_obj.data.user_value;
+			var placeholder = node_obj.data.placeholder;
+			if (this.lang) {
+				node_obj.data.user_value.forEach(element => {
+					value = element.text;
+					html += '<div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text">' + element.id + '</span></div>';
+					html += '<input type="text" class="form-control form-node" data-nodeid="' + this.id + '" placeholder="' + placeholder + '" value = "' + value + '" ></div>';
+				});
+			} else {
+				html += '<input type="text" class="form-control form-node" data-nodeid="' + this.id + '" name="' + this.id + '" id="' + this.id +
+					'" placeholder="' + placeholder + '" value = "' + value + '" >';
+			}
+		}
+		if (this.type === 'options') {
+			node_obj.children.forEach(element => {
+				html += '<select class="custom-select form-node" data-nodeid="' + this.id + '" name="' + this.id + '" id="' + this.id + '">';
+				element.children.forEach(element => {
+					html += '<option value="' + element.text + '">' + element.text + '</option>';
+				});
+				html += '</select>';
+			});
+		}
+		html += '</div>';
+	});
+	html += '</form>';
+	$('.field-options').html(html);
 }
 
 function cosntructFieldsSettings() {
-  $.getJSON('projects/fieldSettings/_construct.json', function (data) {
-    data.forEach(element => {
-      $.getJSON('projects/fieldSettings/' + element + '.json', function (data) {
-        fields_settings.push(data);
-      });
-    });
-  });
-  setTimeout(function () {
-    console.log(JSON.stringify(fields_settings));
-  }, 1500);
+	$.getJSON('projects/fieldSettings/_construct.json', function (data) {
+		data.forEach(element => {
+			$.getJSON('projects/fieldSettings/' + element + '.json', function (data) {
+				fields_settings.push(data);
+			});
+		});
+	});
+	setTimeout(function () {
+		fields_settings = mySort(fields_settings);
+	}, 500);
+}
+
+function updateTree() {
+	var tree = $('#project_tree').jstree(true);
+	var treeData = getJsonNode();
+	tree.settings.core.data = treeData;
+}
+
+function constructTree(data) {
+	$('#project_tree')
+		.jstree({
+			"core": {
+				"data": data,
+				'check_callback': function (o, n, p, i, m) {
+
+					if (m && m.dnd && m.pos !== 'i') {
+						return false;
+					}
+					if (o === "move_node" || o === "copy_node") {
+						if (this.get_node(n).parent === this.get_node(p).id) {
+							return false;
+						}
+					}
+					return true;
+				}
+			},
+			"types": {
+				"#": {
+					"max_children": 3,
+					"icon": "fas fa-project-diagram"
+				},
+				"group": {
+					"max_children": 20,
+					"max_depth": 20,
+					"icon": "far fa-object-group"
+				},
+				"table": {
+					"max_children": 20,
+					"max_depth": 20,
+					"icon": "fas fa-table"
+				},
+				"field": {
+					"max_children": 20,
+					"max_depth": 20,
+					"icon": "fas fa-stream"
+				},
+				"field-setting": {
+					"max_children": 20,
+					"max_depth": 20,
+					"icon": "fa fa-cog"
+				}
+			},
+			'contextmenu': {
+				'items': function (node) {
+					return contextMenu(node, this);
+				}
+			},
+			"plugins": ["dnd", "search", "state", "types", "contextmenu"]
+		})
+		.on('create_node.jstree', function (e, data) {
+			var error = true;
+			var parentData = getJsonNode(data.node.parent);
+			if (parentData.type === 'table' && data.node.type === 'field' && error) {
+				error = false;
+			}
+			if (parentData.type === 'group' && data.node.type === 'table' && error) {
+				error = false;
+			}
+			if (parentData.type === '#' && data.node.type === 'group' && error) {
+				error = false;
+			}
+			if (parentData.type === 'field' && data.node.type === 'field-setting' && error) {
+				error = false;
+			}
+			if (parentData.type === 'field-setting' && data.node.type === 'default' && error) {
+				error = false;
+			}
+
+			if (error) {
+				data.instance.refresh();
+				console.log("can't add " + data.node.type + " into " + parentData.type);
+			} else {
+				data.instance.set_id(data.node, data.node.id);
+			}
+		})
+		.on('changed.jstree', function (e, data) {
+			//console.log(data);
+			if (data.action === "select_node") {
+				var tree = $('#project_tree').jstree(true);
+				var childrens = tree.get_children_dom(data.node.id);
+				var text = '<span class = "right badge badge-success" title="type">' + data.node.type +
+					'</span><span class="right badge badge-danger" title="Name">' + data.node.text +
+					'</span><span class="right badge badge-warning" title="ID">' + data.node.id + '</span>';
+				$('.card-title').html(text);
+				constructForm(childrens);
+			}
+		})
+		.on('create_node.jstree', function (e, data) {
+			updateTree();
+		});
 }
